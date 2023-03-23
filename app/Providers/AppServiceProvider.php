96dc541b68ebd8730a989cf783f8825307b527b4
DESKTOP-073G8KI\kudin <fkudin.tajuddin@gmail.com>
2023-03-24 06:25:23 +0800

    Revert AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,20 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (config('app.vercel.enabled')) {
-            $paths = [
-                '/tmp/framework/sessions',
-                '/tmp/framework/cache',
-                '/tmp/storage/bootstrap/cache',
-                '/tmp/storage/framework/cache',
-                config('view.compiled'),
-            ];
-
-            foreach ($paths as $path) {
-                if (! is_dir($path)) {
-                    mkdir($path, 0755, true);
-                }
-            }         
+        //
     }
 
     /**
